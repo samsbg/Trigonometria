daf2a2ac4ae3264ab1ea1d5c1d136dd35a393dc4
unknown <samanthasofiabg@gmail.com>
2020-10-05 15:20:40 -0500

    Empiezo segunda pagina

--- Trigonometria.py
+++ Trigonometria.py
@@ -10,14 +10,17 @@ from random import randint
 from PIL import ImageTk, Image
 import os
 
-ort = tk.Tk()
-ort.title("Trogonometría")
-ort.geometry("900x500")
-#ort.resizable(width=False, height=False)
-ort.configure(bg="#DEF0D9")
+trig = tk.Tk()
+trig.title("Trogonometría")
+trig.geometry("900x500")
+#trig.resizable(width=False, height=False)
+trig.configure(bg="#DEF0D9")
 
 def segundaPagina():
-    print("Siguiente pagina")
+    titulo.grid(column=3, row=0, columnspan=15, rowspan=2, sticky="WNSE")
+
+    for y in range(15):
+        trig.grid_rowconfigure(y, weight=1)
 
 def primeraPagina():
 
@@ -29,13 +32,21 @@ def primeraPagina():
     regreso.configure(command=regresar)
 
     def irSiguiente():
+        aprende.grid_forget()
+        triangulolabel.grid_forget()
+        reglas.grid_forget()
+        xLabel.grid_forget()
+        yLabel.grid_forget()
+        zLabel.grid_forget()
+        ejemploLabel.grid_forget()
+        ejemploEntry.grid_forget()
+
         segundaPagina()
     
     siguiente.configure(command=irSiguiente)
     siguiente.grid(column = 15, row = 0, rowspan = 2, columnspan = 3, sticky = "NSWE")
 
     regreso.configure(command=regresar)
-    regreso.grid(column=0, row=0, columnspan=3, rowspan=2, sticky="NSWE")
 
     reglas = tk.Label(justify = LEFT, padx = 10, bg="#DEF0D9", font=(None, 18), text="\nsen(A) = cateto opuesto/hipotenusa = z/x\ncos(A) = cateto adyacente/hipotenusa = y/x\ntan(A) = cateto opuesto/cateto adyacente = z/y\n\nCateto opuesto = lado opuesto al ángulo\nCateto adyacente = lado al lado del ángulo\nHipotenusa = lado más largo del triángulo")
     reglas.grid(column = 0, row = 4, columnspan = 12, rowspan = 6, sticky = "N")
@@ -44,8 +55,8 @@ def primeraPagina():
     aprende.grid(row = 2, column = 6, columnspan = 6, rowspan = 2)
 
     triangulo = ImageTk.PhotoImage(Image.open(os.path.abspath(
-        "Triangulo1.png")).resize((230, 230), Image.ANTIALIAS))
-    triangulolabel = tk.Label(ort, image=triangulo, bg="#DEF0D9")
+        "Triangulo1.png")).resize((200, 200), Image.ANTIALIAS))
+    triangulolabel = tk.Label(trig, image=triangulo, bg="#DEF0D9")
     triangulolabel.image = triangulo
     triangulolabel.grid(column = 13, row = 5, rowspan = 4, columnspan = 4)
 
@@ -61,22 +72,23 @@ def primeraPagina():
     ejemploLabel = tk.Label(text="sen(A) =", bg="#DEF0D9", font=(None, 20))
     ejemploLabel.grid(column = 12, row = 10, columnspan = 3, rowspan = 2, sticky="E")
 
-    ejemploEntry = Entry(ort, width=3, font=(None,20))
+    ejemploEntry = Entry(trig, width=3, font=(None,20))
     ejemploEntry.insert(END,"z/x")
     ejemploEntry.configure(state="disabled")
     ejemploEntry.grid(column = 15, row = 10, columnspan = 3, rowspan = 1, sticky="SW")
 
     for y in range(12):
-        ort.grid_rowconfigure(y, weight=1)
+        trig.grid_rowconfigure(y, weight=1)
 
     for x in range(18):
-        ort.grid_columnconfigure(x, weight=1)
+        trig.grid_columnconfigure(x, weight=1)
 
 
 siguiente = tk.Button(text=" 🡆 ", bg="#5BB346", fg="white", font=(None, 20))
 regreso = tk.Button(text=" 🡄 ", bg="#5BB346", fg="white", font=(None, 20))
-titulo = tk.Label(justify = LEFT, text="Ortografía", bg="#5BB346", fg="white", font=(None, 20))
+regreso.grid(column=0, row=0, columnspan=3, rowspan=2, sticky="NSWE")
+titulo = tk.Label(justify = LEFT, text="Trigonometría", bg="#5BB346", fg="white", font=(None, 20))
 
 primeraPagina()
 
-ort.mainloop()
+trig.mainloop()
